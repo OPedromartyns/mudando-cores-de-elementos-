9f22efb9a7a6ad31e449024ad8ce5028c508a061
OPedromartyns <1999martyns@gmail.com>
2023-03-13 21:39:12 -0300

    funçao

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById("btn-amarelo").addEventListener("click", function(){
+    document.querySelector("img").setAttribute("class","amarelo");
+  })
+  document.getElementById("btn-verde").addEventListener("click", function(){
+    document.querySelector("img").setAttribute("class","verde");
+  })
+  
+  document.getElementById("btn-azul").addEventListener("click", function(){
+    document.querySelector("img").setAttribute("class","azul");
+  })
+  document.getElementById("btn-roxo").addEventListener("click", function(){
+    document.querySelector("img").setAttribute("class","roxo");
+  })
